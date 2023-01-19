bddfe6f4bb594f8d0bddc007ab0c1ba8cad12e88
Diego <diegoyon@outlook.com>
2023-01-19 17:22:44 -0600

    add styles to home page

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -4,7 +4,15 @@ import './Home.css';
 const Home = () => {
   return (
     <section className="home">
-      <div></div>
+      <div className="content">
+        <h1>THIS PLACE WILL CHANGE YOU</h1>
+        <p>
+          El ejercicio es el elixir de la vida, y los beneficios de CrossFit se
+          transfieren a actividades diarias como cargar las compras del
+          supermercado hasta superar desafíos en el trabajo.
+        </p>
+        <button>CLASE DE PRUEBA</button>
+      </div>
     </section>
   );
 };
