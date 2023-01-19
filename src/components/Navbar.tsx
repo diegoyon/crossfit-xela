a12f44af541cd985e9fddfde9774b44c76db3924
Diego <diegoyon@outlook.com>
2023-01-19 15:43:55 -0600

    add hamburger icon with animation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,11 +3,19 @@ import { NavLink, Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
+  const handleClick = (event: React.BaseSyntheticEvent) => {
+    event.currentTarget.classList.toggle('open');
+  };
+
   return (
     <nav>
       <h2>CROSSFIT XELA</h2>
       <ul className="links">
-        <li>Hamburger</li>
+        <div id="hamburger-icon" onClick={handleClick}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </ul>
     </nav>
   );
