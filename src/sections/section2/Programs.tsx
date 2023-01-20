1bdd9a3ea8a9c9c178e29ca19cea3297bda82bd4
Diego <diegoyon@outlook.com>
2023-01-20 10:49:03 -0600

    add cards

--- src/sections/section2/Programs.tsx
+++ src/sections/section2/Programs.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import './Programs.css';
 
 const Programs = () => {
   return (
-    <section>
-      <div>Programs</div>
+    <section className="programs">
+      <div className="program-card">CrossFit</div>
+      <div className="program-card">HIIT</div>
+      <div className="program-card">Clases Personales</div>
     </section>
   );
 };
