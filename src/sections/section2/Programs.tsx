ebcda6424a2ffabbda5846f9c87f90a5b23a87e1
Diego <diegoyon@outlook.com>
2023-01-20 11:40:42 -0600

    add styles to section2

--- src/sections/section2/Programs.tsx
+++ src/sections/section2/Programs.tsx
@@ -4,9 +4,37 @@ import './Programs.css';
 const Programs = () => {
   return (
     <section className="programs">
-      <div className="program-card">CrossFit</div>
-      <div className="program-card">HIIT</div>
-      <div className="program-card">Clases Personales</div>
+      <h2>Clases</h2>
+      <div className="program-card">
+        <div className="card-info">
+          <h3>CrossFit</h3>
+          <p>
+            Forma parte de clases grupales que consisten en ejercicios
+            constantemente variados, ejecutados a alta intensidad.
+          </p>
+          <button>Más informacion</button>
+        </div>
+      </div>
+      <div className="program-card">
+        <div className="card-info">
+          <h3>HIIT</h3>
+          <p>
+            Nuestro programa de HIIT te hará quemar calorias y te mantendra en
+            forma con movimientos funcionales.
+          </p>
+          <button>Más informacion</button>
+        </div>
+      </div>
+      <div className="program-card">
+        <div className="card-info">
+          <h3>Clases Personales</h3>
+          <p>
+            ¿Necesitas algo más personalizado? Nuestros coaches están
+            capacitados para lo que necesites.
+          </p>
+          <button>Más informacion</button>
+        </div>
+      </div>
     </section>
   );
 };
