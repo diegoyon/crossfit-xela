2c2917441e9fc4877761039074453ccc67e0fd6b
Diego <diegoyon@outlook.com>
2023-02-09 16:59:40 -0600

    add footer

--- src/sections/section5/Footer.tsx
+++ src/sections/section5/Footer.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import cfxela from '../../images/cfxela.png';
 import './Footer.css';
+import { BsFacebook, BsInstagram } from 'react-icons/bs';
 
 const Footer = () => {
   return (
     <footer className="footer">
       <img src={cfxela} alt="logo blanco" />
-
+      <div className="social">
+        <BsFacebook className="icon" size={25} />
+        <BsInstagram className="icon" size={25} />
+      </div>
       <p>2023 CrossFit Xela. All rights reserved.</p>
     </footer>
   );
