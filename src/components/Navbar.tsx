f6b4f55d56fb1a90e89bec479c861695136a4b2b
Diego <diegoyon@outlook.com>
2023-02-13 13:20:25 -0600

    add sliding navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import { NavLink, Link } from 'react-router-dom';
 import './Navbar.css';
 import logo from '../images/logo.png';
 
 const Navbar = () => {
+  const [mobileMenu, setMobileMenu] = useState(false);
+
+  const showMobileMenu = () => setMobileMenu(!mobileMenu);
+
   const handleClick = (event: React.BaseSyntheticEvent) => {
+    showMobileMenu();
     event.currentTarget.classList.toggle('open');
   };
 
@@ -18,6 +23,16 @@ const Navbar = () => {
           <span></span>
         </div>
       </ul>
+
+      <div className={mobileMenu ? 'nav-menu activated' : 'nav-menu'}>
+        <ul className="nav-menu-items">
+          <li className="navbar-toggle" onClick={handleClick}>
+            {/* <AiOutlineClose className="x-icon" /> */}
+            <p>X</p>
+          </li>
+          <p>list of items</p>
+        </ul>
+      </div>
     </nav>
   );
 };
