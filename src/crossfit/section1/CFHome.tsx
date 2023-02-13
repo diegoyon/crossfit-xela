f4693f97e9e7cc6f7afba2577e43afefad90d43c
Diego <diegoyon@outlook.com>
2023-02-13 11:39:42 -0600

    add home page for crossfit

---
+++ src/crossfit/section1/CFHome.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './CFHome.css';
+
+const CFHome = () => {
+  return (
+    <section className="CFHome">
+      <div className="black-background">
+        <div className="content">
+          <h1>CROSSFIT</h1>
+          <p>
+            El ejercicio es el elixir de la vida, y los beneficios de CrossFit
+            se transfieren a actividades diarias desde cargar las compras del
+            supermercado hasta superar desafíos en el trabajo.
+          </p>
+          <button>CLASE DE PRUEBA</button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default CFHome;
