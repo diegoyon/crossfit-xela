343dd71d0cd3e774bbe7eed10b2c4be192da1d5f
Diego <diegoyon@outlook.com>
2023-02-15 11:04:46 -0600

    add responsive to services

--- src/HomePage/section2/Programs.tsx
+++ src/HomePage/section2/Programs.tsx
@@ -11,15 +11,16 @@ const Programs = () => {
         Sin importar tu edad o nivel de fitness, nosotros te ayudamos a mejorar
         tu salud.
       </p>
-
-      {CardsContent.map((card) => (
-        <Cards
-          key={card.title}
-          title={card.title}
-          description={card.description}
-          link={card.link}
-        />
-      ))}
+      <div className="services">
+        {CardsContent.map((card) => (
+          <Cards
+            key={card.title}
+            title={card.title}
+            description={card.description}
+            link={card.link}
+          />
+        ))}
+      </div>
     </section>
   );
 };
