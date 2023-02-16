9f74176af240e0241117ab50021987371112708d
Diego <diegoyon@outlook.com>
2023-02-16 16:49:14 -0600

    add content and styles to prices

--- src/components/PriceCard/PriceCard.tsx
+++ src/components/PriceCard/PriceCard.tsx
@@ -6,11 +6,46 @@ const PriceCard = () => {
   return (
     <>
       <div className="price-card">
-        <h3>3 veces por semana</h3>
+        <h3>Tres veces por semana</h3>
+        <p>
+          Membresía para las personas que buscan más flexibilidad por límite de
+          tiempo.
+        </p>
         <h4>
-          <span>Q.</span>355
+          Q.355
+          <span className="monthly">/mensuales</span>
         </h4>
-        <p>mensuales</p>
+        <ul>
+          <li>Incluye:</li>
+          <li>
+            Tres clases grupales a la semana en cualquiera de los horarios.
+          </li>
+          <li>Acceso al Open Gym antes o después de la clase.</li>
+          <li>
+            Acceso a ZenPlanner donde podrás darle seguimiento a tus entrenos y
+            guardar tus progreso.
+          </li>
+        </ul>
+      </div>
+      <div className="price-card">
+        <h3>Ilimitado</h3>
+        <p>
+          Membresía para las personas que buscan comprometerse en buscar una
+          rutina de todos los días
+        </p>
+        <h4>
+          Q.425
+          <span className="monthly">/mensuales</span>
+        </h4>
+        <ul>
+          <li>Incluye:</li>
+          <li>Clases grupales ilimitadas en cualquiera de los horarios.</li>
+          <li>Acceso ilimitado al Open Gym a cualquier horario.</li>
+          <li>
+            Acceso a ZenPlanner donde podrás darle seguimiento a tus entrenos y
+            guardar tus progreso.
+          </li>
+        </ul>
       </div>
     </>
   );
