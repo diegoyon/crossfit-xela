6da01e109703f02b27613e102eff266894317d9a
Diego <diegoyon@outlook.com>
2023-03-07 11:23:39 -0600

    add animation to title in the home page

--- src/HomePage/section2/Programs.tsx
+++ src/HomePage/section2/Programs.tsx
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './Programs.css';
 import CardsContent from '../../components/CardsContent';
 import Cards from '../../components/Cards';
 
 const Programs = () => {
+  const [showClass, setShowClass] = useState('');
+  const hiddenRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        // console.log(entry);
+        if (entry.isIntersecting) {
+          setShowClass('show');
+        } else {
+          setShowClass('');
+        }
+      });
+    });
+
+    if (hiddenRef.current) {
+      observer.observe(hiddenRef.current);
+    }
+
+    // cleanup function to disconnect the observer
+    return () => observer.disconnect();
+  }, [hiddenRef]);
+
   return (
     <section className="programs">
-      <h2>¿QUÉ OFRECEMOS?</h2>
-      <p>
+      <h2 ref={hiddenRef} className={`hidden ${showClass}`}>
+        ¿QUÉ OFRECEMOS?
+      </h2>
+      <p className={`hidden ${showClass}`}>
         Sin importar tu edad o nivel de fitness, nosotros te ayudamos a mejorar
         tu salud.
       </p>
