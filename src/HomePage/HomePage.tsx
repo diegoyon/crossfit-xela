36000b8e283151ed1304cee742bb5598b991c863
Diego <diegoyon@outlook.com>
2023-03-07 10:00:18 -0600

    change xela to quetzaltenango

--- src/HomePage/HomePage.tsx
+++ src/HomePage/HomePage.tsx
@@ -12,7 +12,7 @@ const HomePage = () => {
     <>
       <Home
         title="THIS PLACE WILL CHANGE YOU"
-        description="Con más de 7 años de experiencia, CrossFit Xela es el único gimnasio afiliado en Xela. Contamos con 500 metros cuadrados de espacio haciéndonos el gimnasio de CrossFit más grande y completo de la región. Contamos con entrenadores certificados CrossFit L1 que te ayudarán a tener un estilo de vida de lo más saludable y pleno posible."
+        description="Con más de 7 años de experiencia, CrossFit Xela es el único gimnasio afiliado en Quetzaltenango. Contamos con 500 metros cuadrados de espacio haciéndonos el gimnasio de CrossFit más grande y completo de la región. Contamos con entrenadores certificados CrossFit L1 que te ayudarán a tener un estilo de vida de lo más saludable y pleno posible."
         image={portada}
       />
       <Programs />
